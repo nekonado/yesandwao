8126e02eea875ef339b0c95293cf744f4d1c5443
queuek <lahass1213@gmail.com>
2022-12-21 01:13:20 +0900

    npm scripts追記

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
 module.exports = {
   // メインとなるJavaScriptファイル（エントリーポイント）
   //   entry: `./src/index.js`,
-  entry: `./dist/index.js`,
+  entry: "./dist/index.js",
   // ファイルの出力設定
   output: {
     //  出力ファイルのディレクトリ名
     path: `${__dirname}/dist`,
     // 出力ファイル名
     filename: "main.js",
+  },
+  // mode: "development",
+  mode: "production",
+  // ローカル開発用環境を立ち上げる
+  // 実行時にブラウザが自動的に localhost を開く
+  devServer: {
+    static: "dist",
+    open: true,
   },
 };
